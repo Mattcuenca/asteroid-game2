2b79f45412773d700f401f13edc90e27aeec2a4a
Mattcuenca <Mattcuenca@users.noreply.github.com>
2018-06-24 13:51:18 -0700

    parallax.gd arrow press fix
    turns every direction perfectly

--- PARALLAX.gd
+++ PARALLAX.gd
@@ -1,22 +1,44 @@
+
 extends Node2D
 
-const MOVEMENT_SPEED = 50 # pixels per second
+const MOVEMENT_SPEED = 500# pixels per second
 
 var sprite_node
+var sp2_node
 
 func _ready():
     sprite_node = get_node("Ship")
+    sp2_node = get_node("Ship2")
 
 func _process(delta):
-    var input_direction = 0 # 0 is no movement, 1 is right, -1 is left
+    var direction = Vector2()
     
+
     if Input.is_action_pressed("ui_left"):
-        input_direction = -1
+             sp2_node.hide()
+             sprite_node.show()
+             direction.x = -1.0
+             sprite_node.set_flip_v( true )
     elif Input.is_action_pressed("ui_right"):
-        input_direction = 1
-		
-    # notice that the child is moved here
-    sprite_node.position.x += input_direction * MOVEMENT_SPEED * delta
-	
-	
+             sp2_node.hide()
+             sprite_node.show()
+             direction.x = 1.0
+             sprite_node.set_flip_v( false )
+    if Input.is_action_pressed("ui_up"):
+             sp2_node.show()
+             sprite_node.hide()
+             direction.y = -1.0
+             sp2_node.set_flip_v( false )
+    elif Input.is_action_pressed("ui_down"):
+             sp2_node.show()
+             sprite_node.hide()
+             direction.y = 1.0
+             sp2_node.set_flip_v( true )
+
+        
+          
+        
+    var velocity = direction * MOVEMENT_SPEED * delta;
+    position += velocity
+
 	
